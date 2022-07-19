74ccff16f53939d95bc67b19870939b56ecb44ba
achiengcatherine <achiengcatherine431@gmail.com>
2022-07-19 21:54:53 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" >
+<child @updateTitle="updateTitle($event)" v-bind:title="title"></child>
+    <ul type="none">
+      <li>stationery</li>
+      <li>food items</li>
+      <li>clothes</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import child from "@/components/child.vue";
+export default {
+  name: 'App',
+  components: {
+    child
+  },
+  data() {
+    return {
+      title: 'My shopping list'
+    }
+  },
+
+  methods:{
+    updateTitle(title){
+      this.title=title
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <button @click="updateTitle()">Add new list</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'childItem',
+  props:{
+    title:String
+  },
+
+  methods:{
+    updateTitle(){
+      this.$emit('updateTitle','Add more items');
+    },
+  },
+};
+</script>
